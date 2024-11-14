0ca60f30e4c361907aa4a39702e8f4f8660cc2f0
Anastasia <n05012007@gmail.com>
2024-11-14 19:00:57 +0100

    update all

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,15 @@
-function checkForSpam(message) {
-  const checkMessage = message.toLowerCase();
-  if (checkMessage.includes("sale") || checkMessage.includes("spam")) {
-    return("true");
-  } else {
-    return("false");
+function filterArray(numbers, value) {
+  const massiv = [];
+  for(const i of numbers) {
+    if(i > value) {
+       massiv.push(i);
+    }
   }
-
+  return massiv;
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
